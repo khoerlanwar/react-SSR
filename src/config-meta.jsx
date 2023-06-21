cf71385f7eaf85896d5174f5a05a90d4c760a0fe
Khoirul Anwar <61571418+khoerlanwar@users.noreply.github.com>
2023-06-21 11:56:04 +0700

    update meta image

--- src/config-meta.jsx
+++ src/config-meta.jsx
@@ -4,12 +4,12 @@ export default function SEOConfiguration({ title, description, image, url }) {
   return (
     <Helmet>
       <title>{title}</title>
-      <meta name="title" content={title} data-react-helmet="true" />
-      <meta name="description" content={description} data-react-helmet="true" />
-      <meta property="og:title" content={title} data-react-helmet="true" />
-      <meta property="og:description" content={description} data-react-helmet="true" />
-      <meta property="og:image" content={`https://beta-nmwclinic.co.id/${image}`} data-react-helmet="true" />
-      <meta property="og:url" content={'https://beta-nmwclinic.co.id'} data-react-helmet="true" />
+      <meta name="title" content={title} />
+      <meta name="description" content={description} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content="https://dummyimage.com/300.png/09f/fff" />
+      <meta property="og:url" content={'https://beta-nmwclinic.co.id'} />
     </Helmet>
   )
 }
